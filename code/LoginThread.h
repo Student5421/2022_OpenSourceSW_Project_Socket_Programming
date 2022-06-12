f60e7e8c4c58979f938eb534415bbb40ca08881b
SyuA <45464572+SyuA0529@users.noreply.github.com>
2022-06-12 22:38:06 +0900

    변경된 사항에 맞춰서 LoginThread.h 구조 변경

--- code/LoginThread.h
+++ code/LoginThread.h
@@ -19,54 +19,49 @@ void LoginThread(void *client_fd) {
 	int client_fd = shared_data->sockfd;
 	char message[MAX_BUF]; memset(message, 0x00, sizeof(message));
 
-	if(!SendMessageToClient(client_fd, "ReadyToLogin")); //if false, quit login thread
-	if(!ReceiveMessageFromClient(client_fd, message)); //if false, quit login thread
+	while(1) {
+		if(!ReceiveMessageFromClient(client_fd, message)); //if false, quit login thread
+		
+		//check id exist server_data array
+		int id_exist = 0; 
 
-	//if service message is 1, 2, ...
-	int select_service = atoi(message);
+		//if exist
+		if(id_exist) if(!SendMessageToClient(client_fd, "AlreadyExist"));
 
-	switch(select_service) {
-		case 2: //sign up
-			//get ip and passwd
+		//if not
+		else {
+			if(!SendMessageToClient(client_Fd, "CanUseId"));
+			break;
+		}
+	}
 
-			//check there are not same to another client
+	//get clinet service about room
+	int room_service = 0;
+	int success = 0;
+	
+	while(1) {
+		memset(message, 0x00, sizeof(message));
+		if(!ReceiveMessageFromClient(client_fd, message));
+		room_service = atoi(room_service);
 
-			//if same, ask again
+		switch(room_service) {
+			case 1:
+				//create room
+				success = 1;
+				break;
 
-			//if does not same, go to login service
+			case 2:
+				//enter room
+				success = 1;
+				break;
 
-		case 1: //login
-			//ask id and passwd
+			default:
+				//nothing
+		}
 
-			//check id and passwd is in client_database
-
-			//if exist, success to login -> add to server_data
-
-			//if does not exist, fail to login
-
-		case default: //not declare
-			//go to ask login or sign up
+		if(success) break;
 	}
 
 	memset(message, 0x00, sizeof(message));
 	if(!SendMessageToClient(client_fd, "ReadyToRoomService")); //if false, quit thread
-	if(!ReceiveMessageFromClient(client_fd, message)); //if false, quit thread
-
-	select_service = atoi(message);
-	switch(select_service) {
-		case 1: //create room
-			//if finish create room, client enter created room
-
-		case 2: //enter room
-			//ask room number or id and get it
-
-			//check room is exist
-
-			//if exist, enter the room
-
-			//if does not exist, ask room number or id again
-
-		case default: //not declare
-			//go to ask room service
-	}
 }
